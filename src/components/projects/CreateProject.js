f0b5d86780d99c6dbf472e50fa99b9f6f9b6d027
David Nwakaihe <dnwakaihe@gmail.com>
2021-06-22 04:24:27 +0100

    Added Image and video upload

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -5,18 +5,48 @@ import { createProject } from '../../redux/actions/projectActions'
 class CreateProject extends Component {
 state = {
     title: '',
-    content: ''
-  }
+    content: '',
+    imageData: [],
+    imageUrl: '',
+    videoData: [],
+    videoUrl: ''
+}
+
 handleChange = (e) => {
   this.setState({
     [e.target.id]: e.target.value
   })
 }
+
 handleSubmit = (e) => {
   e.preventDefault();
   this.props.createProject(this.state)
 }
+
+handleImageUpload = (e) => {
+  const values = e.target.files;
+  [...values].forEach(file => {
+    console.log(file)
+  this.setState({ imageData: file, imageUrl: URL.createObjectURL(file)})
+  })
+  // this.setState({ data: e.target.files[0]});
+}
+
+
+handleVideoUpload = (e) => {
+  const values = e.target.files;
+  [...values].forEach(file => {
+    console.log(file)
+  this.setState({ videoData: file, videoUrl: URL.createObjectURL(file)})
+  })
+}
+
+// componentDidMount(){
+//   this.handleImageUpload.bind(this)
+// }
+
 render() {
+  console.log(this.state)
     return (
         <div className="container">
           <form onSubmit={this.handleSubmit} className="white">
@@ -28,6 +58,24 @@ render() {
             <div className="input-field">
               <label htmlFor="content"> Project Content </label>
               <textarea id="content" className="materialize-textarea" onChange={this.handleChange}> </textarea>
+            </div>
+            <div className="file-field input-field">
+            <div class="btn blue">
+                <span>Upload Image</span>
+                <input onChange={this.handleImageUpload} accept="image/*" type="file" multiple/>
+            </div>
+            <div class="file-path-wrapper">
+              <input class="file-path validate" type="text" placeholder="Upload one or more images" />
+            </div>
+            </div>
+            <div className="file-field input-field">
+            <div class="btn blue">
+                <span>Upload Video</span>
+                <input onChange={this.handleVideoUpload} accept="video/*" type="file" multiple />
+            </div>
+            <div class="file-path-wrapper">
+              <input class="file-path validate" type="text" placeholder="Upload one or more videos" />
+            </div>
             </div>
             <div className="input-field">
               <button className="btn pink lighten-1 z-index-0">Create</button>
